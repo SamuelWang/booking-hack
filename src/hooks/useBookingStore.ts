c362cf7518616ad7e8b269437a426f20660922aa
Samuel Wang <chieh0919@gmail.com>
2022-10-16 10:13:45 +0800

    add useBookingStore

---
+++ src/hooks/useBookingStore.ts
@@ -0,0 +1,25 @@
+import { useReducer } from 'react';
+import { BookingStoreActionType } from '../enums/bookingStoreActionType';
+import { BookingState, Store, StoreAction } from '../interfaces';
+
+function reducer(
+  state: BookingState,
+  action: StoreAction<BookingStoreActionType>
+) {
+  return Object.assign({}, state);
+}
+
+export const initialState: BookingState = {
+  selectedBookingConfig: '',
+};
+
+export function useBookingStore(): Store<BookingState, BookingStoreActionType> {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return {
+    getState: () => Object.assign({}, state),
+    dispatch: (action: StoreAction<BookingStoreActionType>) => {
+      return dispatch(action);
+    },
+  };
+}
